8f4ad529e5e8926657c0b10d79e1ea8cedf3d992
IonDevCode <97814014+IonDevCode@users.noreply.github.com>
2022-01-16 13:57:37 +0200

    Add files via upload

--- src/HelloWorld.php
+++ src/HelloWorld.php
@@ -2,7 +2,7 @@
 
 namespace IonDevCode\HelloWorld;
 
-class HelloWorldProvider
+class HelloWorld
 {
 
 }
---
+++ tests/src/HelloWorldTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace IonDevCode\HelloWorld\Test;
+
+class HelloWorldTest
+{
+
+}
